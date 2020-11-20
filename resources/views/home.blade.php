6febc5d6b012d3680a5cf34db964af03123f9fbb
Juliano Roque Vieira <julianovieira@yahoo.com.br>
2020-11-20 16:55:35 -0300

    Commentando o  codigo php na home para procurar a qtde de votos por ip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,62 +2,63 @@
 
 @section('content')
 
-<div class="container">
-    <div class="">
-        @isset($elections)
-        <h2> Votação em aberto: </h2>
-        <br>
+    <div class="container">
+        <div class="">
+            @isset($elections)
+                <h2> Votação em aberto: </h2>
+                <br>
 
-        <div class="container-fluid">
+                <div class="container-fluid">
 
-            @foreach ($elections as $election)
-            <br>
-                <img src="{{asset('storage/'.$election->image)}}" alt="" class="img-fluid">
-            <br>
+                    @foreach ($elections as $election)
+                        <br>
+                        <img src="{{ asset('storage/' . $election->image) }}" alt="" class="img-fluid">
+                        <br>
 
-            @php
-                $ip = $_SERVER["REMOTE_ADDR"];
-                $votos = App\Models\Vote::where('ip',$ip)->where('election_id',$election->id)->count();
-                $mostrar = $votos < 5;
-            @endphp
+{{--                         @php
+                            $ip = $_SERVER["REMOTE_ADDR"];
+                            $votos = App\Models\Vote::where('ip',$ip)->where('election_id',$election->id)->count();
+                            $mostrar = $votos < 5;
+                        @endphp --}}
+
+                        <h2>{{ $election->name }}</h2>
+
+                            @foreach ($election->singers as $singer)
+                                <div class="col-md-4">
+                                    <div class="card" style="width: 18rem;">
+                                        @isset($singer->image)
+                                            <img src="{{ asset('storage/' . $singer->image) }}" alt=""
+                                                class="img-fluid card-img-top">
+                                        @endisset
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $singer->fullName }}</h5>
+                                            <p class="card-text">{{ $singer->description }}</p>
+                                            @if ($election->isOpen)
+                                                <form action="{{ route('votar') }}" method="POST">
+                                                    @csrf
+                                                    <input type="hidden" name="election" id="election"
+                                                        value="{{ $election->id }}">
+                                                    <input type="hidden" name="singer" id="singer" value="{{ $singer->id }}">
 
 
-            <h2>{{ $election->name }}</h2>
+                                                    @if (!!$election->ip_already_voted && $mostrar)
+                                                        <span>Voce já participou dessa votação</span>
+                                                    @else
+                                                        <button class="btn btn-sm btn-success ml-2">Votar</button>
+                                                    @endif
+                                                </form>
+                                            @else
+                                                <span>Voce poderá votar em breve!</span>
+                                            @endif
 
-                    @foreach ($election->singers as $singer)
-                    <div class="col-md-4">
-                        <div class="card" style="width: 18rem;">
-                            @isset($singer->image)
-                                <img src="{{asset('storage/'.$singer->image)}}" alt="" class="img-fluid card-img-top">
-                            @endisset
-                            <div class="card-body">
-                            <h5 class="card-title">{{ $singer->fullName }}</h5>
-                            <p class="card-text">{{ $singer->description }}</p>
-                            @if ($election->isOpen)
-                            <form action="{{route('votar')}}" method="POST">
-                                @csrf
-                                <input type="hidden" name="election" id="election" value="{{ $election->id }}">
-                                <input type="hidden" name="singer" id="singer" value="{{ $singer->id }}">
-
-
-                                @if(!!$election->ip_already_voted &&  $mostrar)
-                                   <span>Voce já participou dessa votação</span>
-                                @else
-                                   <button class="btn btn-sm btn-success ml-2">Votar</button>
-                                @endif
-                            </form>
-                            @else
-                                <span>Voce poderá votar em breve!</span>
-                            @endif
-
-                            </div>
-                        </div>
-                    </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
                     @endforeach
-            @endforeach
-        </div>
+                </div>
 
-        @endisset
+            @endisset
+        </div>
     </div>
-</div>
 @endsection
